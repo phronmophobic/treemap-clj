d88fabf24d6e175c53af54ccfd746f09902d5780
Adrian Smith <smith.adriane@gmail.com>
2020-07-19 14:02:20 -0700

    initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to treemap-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/treemap_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns treemap-clj.core-test
+  (:require [clojure.test :refer :all]
+            [treemap-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
