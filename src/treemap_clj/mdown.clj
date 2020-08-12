237abc90e285f0345ae896378156f365b6aa46a7
Adrian Smith <smith.adriane@gmail.com>
2020-08-12 09:35:13 -0700

    updating blog html.

--- src/treemap_clj/mdown.clj
+++ src/treemap_clj/mdown.clj
@@ -139,8 +139,8 @@
 
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}] 
-    [:meta {:name "description" :content ""}]
-    [:meta {:name "author" :content ""}]
+    ;; [:meta {:name "description" :content ""}]
+    [:meta {:name "author" :content "Adrian smith"}]
 
     ;; <link rel="icon" href="../../favicon.ico">
     [:title "Treemaps are awesome!"]
