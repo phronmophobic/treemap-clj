973473ca1d9dbf83732235de77e41ad8909ea07c
Adrian Smith <smith.adriane@gmail.com>
2020-08-13 10:08:55 -0700

    updating project version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject treemap-clj "0.2.0-SNAPSHOT"
+(defproject treemap-clj "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
