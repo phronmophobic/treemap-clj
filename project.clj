bf3ddded01903c36a5d7ec438a5b5af2ae6ac053
Adrian Smith <smith.adriane@gmail.com>
2022-02-11 22:35:26 -0800

    Updating deps.

--- project.clj
+++ project.clj
@@ -3,18 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.phronemophobic/membrane "0.9.31.0-beta"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.phronemophobic/membrane "0.9.31.8-beta"]
+                 [com.phronemophobic/viscous "1.1"]
                  [com.github.davidmoten/rtree "0.8.7"]]
   :resource-paths []
   :main ^:skip-aot treemap-clj.core
   ;; :java-cmd "/Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home/bin/java"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/data.json "1.0.0"]
-                                  [com.vladsch.flexmark/flexmark-all "0.62.2"]
+             :dev {:dependencies [[org.clojure/data.json "2.4.0"]
+                                  [com.vladsch.flexmark/flexmark-all "0.64.0"]
                                   [hiccup "1.0.5"]
-                                  [org.clojure/clojurescript "1.10.764"]
-                                  [org.clojure/tools.namespace "1.0.0"]]}
-             :provided {:dependencies [[org.clojure/test.check "0.9.0"]
+                                  [org.clojure/clojurescript "1.11.4"]
+                                  [org.clojure/tools.namespace "1.2.0"]]}
+             :provided {:dependencies [[org.clojure/test.check "1.1.1"]
                                        [com.phronemophobic.membrane/skialib-macosx-aarch64 "0.9.31.0-beta"]]}})
