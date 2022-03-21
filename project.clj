7156d5da7a1d305c8154bdca2c1d6c0478c77229
Adrian Smith <smith.adriane@gmail.com>
2022-03-20 23:16:18 -0700

    Add url and description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.phronemophobic/treemap-clj "0.2.5"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An alternative to pprint for generically visualizing heterogeneous, hierarchical data"
+  :url "https://github.com/phronmophobic/treemap-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
