f567d610058f6c2908e193809704b9d4d5c809a1
Adrian Smith <smith.adriane@gmail.com>
2022-10-09 20:36:05 -0700

    Bumping verison to 0.2.6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.phronemophobic/treemap-clj "0.2.5"
+(defproject com.phronemophobic/treemap-clj "0.2.6"
   :description "An alternative to pprint for generically visualizing heterogeneous, hierarchical data"
   :url "https://github.com/phronmophobic/treemap-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
